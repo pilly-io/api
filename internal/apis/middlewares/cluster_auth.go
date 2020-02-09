8d0456fcfadadd6c60ac8c8f5dc5b4be888656a1
Aurélien <aurelien.amilin@gmail.com>
2020-02-09 17:29:38 +0100

    Add ClusterAuth middleware to authenticate incoming collector requests

---
+++ internal/apis/middlewares/cluster_auth.go
@@ -0,0 +1,31 @@
+package middlewares
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/pilly-io/api/internal/db"
+	"github.com/pilly-io/api/internal/models"
+)
+
+const ClusterAuthHeaderName = "X-API-TOKEN"
+
+// CluserAuthMiddleware authenticates incoming requests by looking at the
+// X-API-TOKEN header and inject the corresponding cluster into the gin context
+func CluserAuthMiddleware(table *db.ClustersTable) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		apiToken := c.Request.Header.Get(ClusterAuthHeaderName)
+		cluster := models.Cluster{}
+		query := db.Query{
+			Conditions: db.QueryConditions{"api_token": apiToken},
+		}
+		if err := table.Find(query, &cluster); err == nil {
+			c.Set("cluster", &cluster)
+			fmt.Println(cluster.Name)
+			c.Next()
+		} else {
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+	}
+}
---
+++ internal/apis/middlewares/cluster_auth_test.go
@@ -0,0 +1,67 @@
+package middlewares
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/pilly-io/api/internal/db"
+	"github.com/pilly-io/api/internal/models"
+)
+
+func TestClustersAuth(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "ClustersAuth Suite")
+}
+
+var _ = Describe("ClustersAuth", func() {
+	var (
+		middleware      gin.HandlerFunc
+		router          *gin.Engine
+		called          bool
+		cluster         *models.Cluster
+		receivedCluster *models.Cluster
+	)
+	BeforeEach(func() {
+		called = false
+		database, _ := db.New("sqlite3", ":memory:")
+		database.Migrate()
+		cluster = &models.Cluster{
+			Name:     "cluster1",
+			APIToken: "1234567",
+		}
+		database.Clusters().Insert(cluster)
+
+		middleware = CluserAuthMiddleware(database.Clusters())
+		router = gin.New()
+		router.Use(middleware)
+		router.GET("/", func(c *gin.Context) {
+			called = true
+			contextValue, _ := c.Get("cluster")
+			receivedCluster = contextValue.(*models.Cluster)
+		})
+	})
+
+	It("should set cluster in context and call handler", func() {
+		w := httptest.NewRecorder()
+		req, _ := http.NewRequest("GET", "/", nil)
+		req.Header.Set(ClusterAuthHeaderName, cluster.APIToken)
+		router.ServeHTTP(w, req)
+
+		Expect(called).To(BeTrue())
+		Expect(receivedCluster.Name).To(Equal(cluster.Name))
+	})
+
+	It("should not call handler if cluster not found", func() {
+		w := httptest.NewRecorder()
+		req, _ := http.NewRequest("GET", "/", nil)
+		req.Header.Set(ClusterAuthHeaderName, "XXXXXX")
+		router.ServeHTTP(w, req)
+
+		Expect(called).To(BeFalse())
+		Expect(w.Result().StatusCode).To(Equal(http.StatusUnauthorized))
+	})
+})
