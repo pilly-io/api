77fb22311c87f4ca870eb3e4d44fce9d74938e9a
py_david <david.macro7@gmail.com>
2020-02-09 16:04:36 +0100

    wip

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/pilly-io/api
 go 1.13
 
 require (
+	github.com/coreos/etcd v3.3.10+incompatible
 	github.com/gin-gonic/gin v1.5.0
 	github.com/google/uuid v1.1.1
 	github.com/jinzhu/gorm v1.9.12
--- internal/apis/clusters_test.go
+++ internal/apis/clusters_test.go
@@ -2,6 +2,7 @@ package apis
 
 import (
 	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -10,6 +11,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/pilly-io/api/internal/db"
+	"github.com/pilly-io/api/internal/models"
 )
 
 func TestRunner(t *testing.T) {
@@ -20,10 +22,11 @@ func TestRunner(t *testing.T) {
 var _ = Describe("Clusters", func() {
 	var (
 		//handler *ClustersHandler
-		engine *gin.Engine
+		engine   *gin.Engine
+		database *db.GormDatabase
 	)
 	BeforeEach(func() {
-		database, _ := db.New("sqlite3", ":memory:")
+		database, _ = db.New("sqlite3", ":memory:")
 		database.Migrate()
 		gin.SetMode(gin.TestMode)
 		engine = gin.New()
@@ -32,12 +35,35 @@ var _ = Describe("Clusters", func() {
 
 	Describe("Create()", func() {
 		It("Should create a record and returns a 201", func() {
+			var paylod jsonFormat
 			res := httptest.NewRecorder()
-			cluster := []byte(`{"name":"cluster1", "provider":"aws"}`)
-			req, _ := http.NewRequest("POST", "/api/v1/clusters", bytes.NewBuffer(cluster))
+
+			//1. Create the POST request
+			clusterBytes := []byte(`{"name":"cluster1", "provider":"aws"}`)
+			req, _ := http.NewRequest("POST", "/api/v1/clusters", bytes.NewBuffer(clusterBytes))
 			engine.ServeHTTP(res, req)
+
+			//2. Analyse the result
+			json.Unmarshal(res.Body.Bytes(), &paylod)
 			Expect(res.Code).To(Equal(201))
-			//Expect(res.Body.String()).To(Equal(`{"name":"cluster1", "provider":"aws"}`))
+			Expect(paylod["data"]).To(HaveKeyWithValue("name", "cluster1"))
+			Expect(paylod["data"]).To(HaveKeyWithValue("provider", "aws"))
+		})
+		It("Should fails because the record already exist", func() {
+			var paylod jsonFormat
+			res := httptest.NewRecorder()
+			cluster1 := models.Cluster{Name: "cluster1"}
+			database.Insert(&cluster1)
+
+			//1. Create the POST request
+			clusterBytes := []byte(`{"name":"cluster1", "provider":"aws"}`)
+			req, _ := http.NewRequest("POST", "/api/v1/clusters", bytes.NewBuffer(clusterBytes))
+			engine.ServeHTTP(res, req)
+
+			//2. Analyse the result
+			json.Unmarshal(res.Body.Bytes(), &paylod)
+			Expect(res.Code).To(Equal(409))
+			Expect(paylod["errors"]).To(HaveLen(1))
 		})
 	})
 })
