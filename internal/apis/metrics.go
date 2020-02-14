3d56a9be9d5fcd737e7c69293d5a4205a3a01c47
py_david <david.macro7@gmail.com>
2020-02-14 19:51:23 +0100

    wip

--- internal/apis/metrics.go
+++ internal/apis/metrics.go
@@ -10,29 +10,43 @@ import (
 	"github.com/pilly-io/api/internal/db"
 )
 
-// OwnersHandler definition
-type OwnersHandler struct {
+// MetricsHandler definition
+type MetricsHandler struct {
 	DB db.Database
 }
 
-// ListMetrics of owners within a period
-func (handler *OwnersHandler) ListMetrics(c *gin.Context) {
-	// 1. Check sanity of the request
+const MinPeriod = 60 // in seconds
+const MetricCPUUsed = "fill_with_proper_cpu_value"
+const MetricMemoryUsed = "fill_with_proper_memory_value"
+
+// ValidateRequest : Validate the cluster and start/end
+func (handler *MetricsHandler) ValidateRequest(c *gin.Context) bool {
 	query := db.Query{
 		Conditions: db.QueryConditions{"id": c.Param("id")},
 	}
 	if !handler.DB.Clusters().Exists(query) {
 		c.JSON(http.StatusNotFound, ErrorsToJSON(errors.New("cluster_does_not_exist")))
-		return
+		return false
 	}
 	start, err := strconv.ParseInt(c.Query("start"), 10, 64)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, ErrorsToJSON(errors.New("invalid_start")))
-		return
+		return false
 	}
 	end, err := strconv.ParseInt(c.Query("end"), 10, 64)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, ErrorsToJSON(errors.New("invalid_end")))
+		return false
+	}
+	c.Set("Start", start)
+	c.Set("End", end)
+	return true
+}
+
+// List the metrics of the cluster within an interval
+func (handler *MetricsHandler) List(c *gin.Context) {
+	// 1. Check sanity of the request
+	if !handler.ValidateRequest(c) {
 		return
 	}
 	period, err := strconv.ParseInt(c.Query("period"), 10, 64)
@@ -40,12 +54,9 @@ func (handler *OwnersHandler) ListMetrics(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, ErrorsToJSON(errors.New("invalid_period")))
 		return
 	}
-	//2. Get the metrics
-	fmt.Println(start)
-	fmt.Println(end)
+	//2. Get the owners within the interval
+	//3. Get the metrics within the interval grouped by period
+	fmt.Println(c.Get("Start"))
+	fmt.Println(c.Get("End"))
 	fmt.Println(period)
-}
-
-// Usage of owners within a period
-func (handler *OwnersHandler) Usage(c *gin.Context) {
 }
